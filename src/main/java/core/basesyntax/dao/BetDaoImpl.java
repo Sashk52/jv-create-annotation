92ed39a6ea303a083b4690736fb5f42ab7afddfd
Sashk52 <a.tsven@gmail.com>
2021-01-08 16:28:00 +0200

    added extra methods in Bet and BetDao

--- src/main/java/core/basesyntax/dao/BetDaoImpl.java
+++ src/main/java/core/basesyntax/dao/BetDaoImpl.java
@@ -3,6 +3,7 @@ package core.basesyntax.dao;
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.Bet;
 import java.util.List;
+import java.util.Optional;
 
 public class BetDaoImpl implements BetDao {
     @Override
@@ -13,5 +14,15 @@ public class BetDaoImpl implements BetDao {
     @Override
     public List<Bet> getAll() {
         return Storage.bets;
+    }
+
+    @Override
+    public void delete(Bet bet) {
+        Storage.bets.remove(bet);
+    }
+
+    @Override
+    public Optional<Bet> get(long id) {
+        return Optional.ofNullable(Storage.bets.get((int) id));
     }
 }
